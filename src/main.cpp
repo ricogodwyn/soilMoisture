cd12a568952c0a6d04e5ceb06f986f884e7500d2
ricogod <125234686+ricogod@users.noreply.github.com>
2024-07-20 14:04:11 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+// put function declarations here:
+#define soilmoistPin A1
+int dry = 294;
+int wet = 670;
+int SoilValue;
+void setup()
+{
+  Serial.begin(9600);
+}
+void soilCount()
+{
+  int soilmoist = analogRead(soilmoistPin);
+  int soilMoistPercent = map(soilmoist, wet, dry, 0, 100);
+  soilMoistPercent = constrain(soilMoistPercent, 0, 100);
+  Serial.print("Soil Moisture: ");
+  Serial.print(soilMoistPercent);
+  // SoilValue = soilMoistPercent;
+  Serial.println("%");
+  delay(1000);
+}
+
+void loop()
+{
+  soilCount();
+}
