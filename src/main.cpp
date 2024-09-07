7f9674afe8b861c0751eaaf31f63cf2254ee17d6
ric <frederico.godwyn@gmail.com>
2024-09-07 18:40:17 +0700

    added logic for relay

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,14 @@
 int dry = 294;
 int wet = 670;
 int SoilValue;
+const int relayPin = 7;
 void setup()
 {
   Serial.begin(9600);
+  pinMode(relayPin, OUTPUT);
 }
 void soilCount()
-{
+{ 
   int soilmoist = analogRead(soilmoistPin);
   int soilMoistPercent = map(soilmoist, wet, dry, 0, 100);
   soilMoistPercent = constrain(soilMoistPercent, 0, 100);
@@ -19,6 +21,15 @@ void soilCount()
   // SoilValue = soilMoistPercent;
   Serial.println("%");
   delay(1000);
+}
+
+void relayDC(){
+  if(SoilValue < 50){
+    digitalWrite(relayPin, HIGH);
+  }
+  else{
+    digitalWrite(relayPin, LOW);
+  }
 }
 
 void loop()
